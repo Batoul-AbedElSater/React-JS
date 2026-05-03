92753d08e6d8e5c1ab700a4835915076a5fca88e
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-03 20:32:19 +0300

    storing html elements in variables

--- index.jsx
+++ index.jsx
@@ -1,6 +1,7 @@
-import { createElement } from 'react'
 import {createRoot} from 'react-dom/client'
 const root = createRoot(document.getElementById('root'))
-const reactElement = createElement('h1',null,'hello world')
+const reactElement = <h1>Hello from JSX!</h1>
 console.log(reactElement)
-root.render(reactElement)
+root.render(
+    reactElement
+)
