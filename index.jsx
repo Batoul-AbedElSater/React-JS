13b0a3633cdaae6948a26a5526eeba7e37827556
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-03 18:51:06 +0300

    setup react from scratch . this time with unordered list

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import {createRoot} from 'react-dom/client'
+createRoot(document.getElementById('root')).render(
+    <ul>
+        <li>Super popular JS library</li>
+        <li>Easy to learn</li>
+        <li>Great community support</li> 
+    </ul>
+)
