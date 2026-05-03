daa3a00bdfdc06e0542fe185b858345454c6bce3
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-03 20:07:11 +0300

    Practicing createElement function

---
+++ index.jsx
@@ -0,0 +1,6 @@
+import { createElement } from 'react'
+import {createRoot} from 'react-dom/client'
+const root = createRoot(document.getElementById('root'))
+const reactElement = createElement('h1',null,'hello world')
+console.log(reactElement)
+root.render(reactElement)
