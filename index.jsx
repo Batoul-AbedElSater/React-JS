4b64751d6b382eba75655d6a8117b5e9bdbbc2e8
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-03 22:00:47 +0300

    React Facts Mini-Project

--- index.jsx
+++ index.jsx
@@ -1,7 +1,18 @@
-import { createElement } from 'react'
+
 import {createRoot} from 'react-dom/client'
 const root = createRoot(document.getElementById('root'))
-const reactElement = createElement('h1',null,'hello world')
-console.log(reactElement)
-root.render(reactElement)
+
+root.render(
+    <main>
+        <img src="react-logo.png" width="40px" alt="React Logo" />
+        <h1>Fun Facts about React!</h1>
+        <ul>
+            <li>Was first release in 2013</li>
+            <li>Is a JavaScript library for building user interfaces</li>
+            <li>Uses a virtual DOM to improve performance</li>
+            <li>Follows a component-based architecture</li>
+            <li>Has a rich ecosystem of tools and libraries</li>
+        </ul>
+    </main>
+)
 
