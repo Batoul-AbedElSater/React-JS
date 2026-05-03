f929b7c26e868dbb4aa905f913645221ce40728b
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-03 21:32:19 +0300

    Imparative VS declarative & composable

--- index.jsx
+++ index.jsx
@@ -1,7 +1,16 @@
 import {createRoot} from 'react-dom/client'
 const root = createRoot(document.getElementById('root'))
-const reactElement = <h1>Hello from JSX!</h1>
-console.log(reactElement)
+//imperative way    
+const h1 = document.createElement('h1')
+h1.textContent = 'Welcome to My Awesome Website'
+h1.className = 'header'
+document.getElementById('root').appendChild(h1)
+//declarative way
 root.render(
-    reactElement
+    <h1 className='header'>Welcome to My Awesome Website</h1>
 )
+// const reactElement = <h1>Hello from JSX!</h1>
+// console.log(reactElement)
+// root.render(
+//     reactElement
+// )
