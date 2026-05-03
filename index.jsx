e44707cc9fd3ceb86b741a3bc4764d4946b9950c
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-03 17:54:35 +0300

    Chalenge: re-write first lines of REACT code

--- index.jsx
+++ index.jsx
@@ -1,6 +1,9 @@
-
-import {createRoot} from 'react-dom/client';
+import {createRoot} from 'react-dom/client'
 //1. create a root 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'))
+
 //2. render some markup to the root
-root.render(<h1>Welcome to React!</h1>)
+root.render(
+    <p>Hello , Welcome to React JS ☺! we are learning React JS with Vite and React Router DOM. React JS is a JavaScript library for building user interfaces. It allows developers to create reusable UI components and manage the state of their applications efficiently. With React, you can build complex and interactive web applications with ease. Happy coding!
+    </p>
+)
