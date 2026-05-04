02eed5b634681535a3dde132bc0069382aaa017b
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-04 20:55:41 +0300

    custom components (parent-child)

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -6,23 +6,41 @@ import heroImg from './assets/hero.png'
 // import index from './index.jsx'
 import {createRoot} from 'react-dom/client'
 const root = createRoot(document.getElementById('root'));
+import { Fragment } from 'react'
 
-function App() {
-
+function Header(){
   return (
-    <div className='App'>
-        <header className='Header'>
-          <h1 className='h1'>My React App</h1>
-        </header>
-        <ol className='Ol'>
+     <header className='Header'>
+          <h1 className='h1'>My React App😎</h1>
+          <img src={heroImg} alt="Hero Image" />
+      </header>
+
+  )
+}
+function Ol(){
+  return (
+    <ol className='Ol'>
           <li className='Li'>Vite Setup</li>
           <li className='Li'>JSX Syntax</li>
           <li className='Li'>React Components</li>
         </ol>
-        <footer className='Footer'>
+  )
+}
+function Footer(){
+  return(
+    <footer className='Footer'>
           <p>© 2023 My React App. All rights reserved.</p>
         </footer>
-    </div>
+  )
+}
+function App() {
+
+  return (
+    <>
+        <Header />
+        <Ol />
+        <Footer />
+    </>
   );
 }
  root.render(
