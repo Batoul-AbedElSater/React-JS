9c2d9bea019ee125013ce17a4b0f38ad52201e0a
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-08 23:26:42 +0300

    make the header for the project

---
+++ first-react/src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import sweet from "../assets/sweet.jpeg"
+export default function Header(){
+    return(
+       <header>
+            <img src={sweet} alt="sweet"/>
+            <h1>A Taste Journey of International Sweets 🍰</h1>
+       </header>
+    )
+}
