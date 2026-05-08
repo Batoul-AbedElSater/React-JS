930b5327219dbc89cb6a528b8cded9f5d0edc710
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-08 18:30:22 +0300

    Creating a new react page that identifies me

---
+++ first-react/src/components/Main.jsx
@@ -0,0 +1,24 @@
+import "../Styles.css"
+
+function Main() {
+  return(
+   <main>
+       <h1>A Girl from the Valley of Vines and Violets ♈</h1>
+       <p>💜Welcome to my website💜</p>
+       <ul>
+        <li>My name is Batoul Abed El Sater, and I am a passionate web developer.</li>
+        <li>I am a third-year computer science student, walking through my very last semester before graduation—code in one hand, dreams in the other.</li>
+        <li>I am a writer who breathes life into Arabic books, weaving worlds from the soul of my language.</li>
+        <li>I come from Lebanon, Baalbek—where history sleeps in stones and rises in my heart.</li>
+        <li>I love spring, because that’s when flowers remember how to bloom, and so do I.</li>
+        <li>My life goal is to stand tall as a businesswoman and a famous writer—both a strategist and a storyteller.</li>
+        <li>I delight in flowers, fresh juices, sun-warmed fruits, and the taste of traditional Lebanese food.</li>
+        <li>My favorite color is purple—the shade of creativity, wisdom, and quiet royalty.</li>
+        <li>I am a deeply conscious girl, who notices the unseen and feels the unspoken.</li>
+        <li>I am a highly organized girl, who builds structure so my dreams have somewhere safe to grow.</li>
+       </ul>
+   </main>
+  )
+}
+
+export default Main;
