47ba28c2c2d8e63738dcf0081b7683e8c2f1d589
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-09 13:36:39 +0300

    using props to reuse the card with its specific details

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,12 +1,36 @@
 import Contact from "./Contact"
-
+import lip  from './assets/lip.jpeg'
+import lip2  from './assets/lip2.jpeg'
+import lip3  from './assets/lip3.jpeg'
+import lip4  from './assets/lip 4.jpeg'
 function App(){
   return (
-    <div className="contacts">
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
+    <div className="cards-wrapper">
+        <Contact
+          img={lip}
+          name = "Lip Gloss"
+          price = "15$"
+          brand="ByBatoul💄"
+
+         />
+        <Contact 
+          img={lip2} 
+          name = "Lip Gloss"
+          price = "20$"
+          brand="RedLips❤"
+        />
+        <Contact 
+          img={lip3}
+          name = "Lip Gloss"
+          price = "25$"
+          brand="B 💕 S"
+        />
+        <Contact 
+           img = {lip4}
+           name = "Lip Gloss"
+           price = "30$"
+           brand="ByBatoul💄"
+        />
         
     </div>
     
--- first-react/src/Contact.jsx
+++ first-react/src/Contact.jsx
@@ -1,18 +1,18 @@
-export default function Contact(){
+
+export default function Contact(props){
+    
     return(
         <article className="contact-card">
-                <img src="" alt=""/>
-                <h3>Lip gloss</h3>
-                <div className="info-group">
-                    <img src="" alt=""/>
-                    <p>333 444 555</p>
+            <img className="card-image" src={props.img} alt="lipgloss"/>
+            <div className="card-content">
+                <h3 className="card-title">{props.name}</h3>
+                <div className="card-details">
+                    <h4>{props.price}</h4>
                 </div>
-                <div className="info-group">
-                    <img src="" alt=""/>
-                    <p>red.lipgloss.bas</p>
+                <div className="card-details">
+                    <p>{props.brand}</p>
                 </div>
-
+            </div>
         </article>
     )
 }
-
