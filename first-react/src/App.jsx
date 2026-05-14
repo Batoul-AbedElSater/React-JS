33a83c71077f7c467b2576dcf2343a47d6d6db5e
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-14 18:09:26 +0300

    showing the elements of the array by a toggling a button using complexStateArray

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import Header from "./Header";
-import MainContent from "./MainContent";
-export default function App() {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-      
-    </div>
-  );
+import React from "react"
+import { useState } from "react"
+export default function App(){
+
+const [myFavoriteThings, setMyFavoriteThings] = React.useState([])
+const allFavoriteThings = ["💜","🎶","🤷‍♀️","🤦‍♀️","♈"]
+
+const thingsElements = myFavoriteThings.map(thing => <p key={thing}>{thing}</p>)
+
+function addFavoriteThing(){
+  setMyFavoriteThings(prevFavThings => [...prevFavThings,allFavoriteThings[prevFavThings.length]])
+}
+
+
+return (
+  <main>
+    <button  onClick={addFavoriteThing}>Add item</button>
+    <section>{thingsElements}</section>
+  </main>
+)
 }
