15eb5c170c122c4b36aba433bc39e36cf663d403
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-14 17:11:43 +0300

    Toggling State

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,13 +1,17 @@
+import React, { useState } from "react"
 export default function App(){
 
-  const isGoingOut = true
+  const[isGoingOut, setIsGoingOut]= React.useState(false)
 
+function changeMind(){
+  setIsGoingOut(prev => !prev)
+}
  
 
   return(
     <main>
        <h1 > Do I feel like going out tonight?</h1>
-       <button>{isGoingOut ? "Yes" : "No"}</button>
+       <button onClick={changeMind}>{isGoingOut ? "Yes" : "No"}</button>
     </main>
   )
 
