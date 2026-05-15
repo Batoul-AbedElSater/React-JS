148dfdd3f5922ef980978d8d940ae726ea1040c0
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-15 14:21:30 +0300

    form submission and action

--- first-react/src/index.jsx
+++ first-react/src/index.jsx
@@ -1,24 +1,52 @@
 import ReactDOM from 'react-dom/client'
+import React from 'react'
 
 function App(){
 
-    function handleClick(){
-        console.log("I was clicked!")
+    // function handleSubmit(event){
+    //     event.preventDefault()
+    //     const forEl = event.currentTarget
+    //     const forData = new FormData(forEl)
+    //     const email = forData.get("email")
+    //     console.log(email)
+        //gather the info from the form
+        //submit it to backend 
+    //     forEl.reset()
+    // }
+
+    function signUp(formData){
+        const email = formData.get("email") //name
+        const password = formData.get("password")
+        
+
     }
 
-    function handleMouseOver(){
-        console.log("I was hovered!")
-    }
     return(
-        <main className='container'>
-           <img 
-                src=''
-                alt=''
-                onMouseOver={handleMouseOver}
-           />
-            <button onClick={handleClick}> Click me </button>
+        <section>
+            <h1>SignUp Form </h1>
+            {/* <form action="phpfile.php" onSubmit={handleSubmit} method="post"> */}
+            <form action={signUp}>
+              
+                <label htmlFor='email'>Email :</label>
+                <input id='email' type='email' name='email' placeholder='Email'/>
+                <br />
 
-        </main>
+                {/* challenge */}
+
+                <label htmlFor='password'>Password:</label>
+                <input id='password' type='password' name='password' placeholder='Password'/>
+
+                <br />
+
+                {/* <input type='submit' value="Click"/> */}
+
+                <button>Submit</button>
+
+                
+            </form>
+
+        </section>
+        
     )
 }
 
