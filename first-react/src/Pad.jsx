065326c170845ae22c5fe26c3b5edc04fef331dd
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-17 12:01:43 +0300

    add on/off property

--- first-react/src/Pad.jsx
+++ first-react/src/Pad.jsx
@@ -1,5 +1,10 @@
 export default function Pad(props){
     return(
-        <button className="pad" style={{backgroundColor: props.color}}></button>
+        <button className="pad" style={{backgroundColor: props.color}}
+        className={props.on ? "on" : undefined}
+        >
+
+        </button>
+
     )
 }
--- first-react/src/pads.jsx
+++ first-react/src/pads.jsx
@@ -1,8 +1,8 @@
 export default [
   { id: 1, color: "red", on: true },
-  { id: 2, color: "green", on: false },
+  { id: 2, color: "green", on: true },
   { id: 3, color: "blue", on: true },
-  { id: 4, color: "yellow", on: true },
+  { id: 4, color: "yellow", on: false },
   { id: 5, color: "pink", on: false },
   { id: 6, color: "purple", on: false }
 ];
