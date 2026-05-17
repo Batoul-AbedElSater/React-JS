9da1d34c794ad9f60df6df1775a58f74863bf80c
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-17 11:50:38 +0300

    sound pads challenge 2 :adding new file Pad

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import padsData from "./pads";
 import "./App.css";
+import Pad from "./Pad"
 
 export default function App() {
   const [pads, setPads] = React.useState(padsData);
 
   const buttonElements = pads.map(pad => (
-    <button
-      key={pad.id}
-      className="pad"
-      style={{ backgroundColor: pad.color }}
-    >
-      {pad.on ? "ON" : "OFF"}
-    </button>
-  ));
+    <Pad key={pad.id} color={pad.color}/>
+  ))
 
   return (
     <main>
---
+++ first-react/src/Pad.jsx
@@ -0,0 +1,5 @@
+export default function Pad(props){
+    return(
+        <button className="pad" style={{backgroundColor: props.color}}></button>
+    )
+}
