53f82a85b3c1bf9176bb05a8c3864cdcef882729
Batoul AS <batoulabdelsater234@gmail.com>
2026-05-17 13:52:28 +0300

    sound pads :local VS shared state

--- first-react/src/Pad.jsx
+++ first-react/src/Pad.jsx
@@ -1,9 +1,13 @@
-export default function Pad(props){
-    return(
-        <button className="pad" style={{backgroundColor: props.color}}
-        className={props.on ? "on" : undefined}
-        >
+import React from "react"
 
+export default function Pad(props){ 
+    return(
+        <button className="pad"
+        style={{backgroundColor: props.color}}
+        className={props.on ? "on" : undefined}
+        onClick={() => props.toggle(props.id)}
+        >
+              
         </button>
 
     )
--- first-react/src/pads.jsx
+++ first-react/src/pads.jsx
@@ -2,7 +2,7 @@ export default [
   { id: 1, color: "red", on: true },
   { id: 2, color: "green", on: true },
   { id: 3, color: "blue", on: true },
-  { id: 4, color: "yellow", on: false },
-  { id: 5, color: "pink", on: false },
+  { id: 4, color: "black", on: false },
+  { id: 5, color: "orange", on: false },
   { id: 6, color: "purple", on: false }
 ];
